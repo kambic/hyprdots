b566b912e70a45f9f023f6c9140b486aa0088939
rok <rok.kambic@telekom.si>
2025-11-17 21:42:04 +0100

    steam v1

--- bin/steam_wr.sh
+++ bin/steam_wr.sh
@@ -1,43 +1,47 @@
 #!/bin/bash
-# Usage: ./wrapper.sh [--disable] [command args...]
 
 ICD_DIR="/usr/share/vulkan/icd.d"
 LOG_FILE="$HOME/work/steam/wrapper.log"
 STAT_FILE="$HOME/work/steam/stat.log"
+
 export MESA_SHADER_CACHE_MAX_SIZE=12G
-# export LD_PRELOAD=""
-# export LD_PRELOAD="/usr/lib/libgamemodeauto.so.0"
 export LD_BIND_NOW=1
-# export RADV_PERFTEST=nggc,gpl,sam
-# export VK_LOADER_LAYERS_ENABLE=VK_LAYER_MANGOHUD_overlay_x86_64
 export VK_DRIVER_FILES="${ICD_DIR}/radeon_icd.i686.json:${ICD_DIR}/radeon_icd.x86_64.json"
 export DISABLE_LAYER_AMD_SWITCHABLE_GRAPHICS_1=1
 
-# Toggle runmode
-DISABLED=false
-if [[ "$1" == "--disable" ]]; then
-  DISABLED=true
-  shift
-fi
-
-# If no command provided, show usage and exit
-if [[ $# -eq 0 ]]; then
-  echo "Usage: $0 [--disable] <command> [args...]"
-  exit 1
-fi
-
-# Log setup
-echo "" >"$LOG_FILE"
-log() {
-  echo "[wrapper] $@" | tee "$LOG_FILE"
+# --- launch presets ---
+show_menu() {
+  printf "%s\n" \
+    "Normal" \
+    "MangoHud" \
+    "Gamescope 1440p" \
+    "Native (Disable Wrapper)" \
+    "Debug VK_LAYER" |
+    rofi -dmenu -i -p "Launch mode"
 }
-# Wrapper
-if $DISABLED; then
-  log "[ DIRECT  RUN ] [cmdline]: $*"
-  "$@" | tee "$LOG_FILE"
-else
-  gamemoderun \
-  gamescope \
+
+preset=$(show_menu)
+[ -z "$preset" ] && exit 0
+
+case "$preset" in
+"Normal") ;; # nothing extra
+"MangoHud")
+  export VK_LOADER_LAYERS_ENABLE=VK_LAYER_MANGOHUD_overlay_x86_64
+  ;;
+"Gamescope 1440p")
+  USE_GAMESCOPE=1
+  ;;
+"Native (Disable Wrapper)")
+  exec "$@"
+  ;;
+"Debug VK_LAYER")
+  export VK_LOADER_LAYERS_ENABLE=VK_LAYER_KHRONOS_validation
+  ;;
+esac
+
+# --- run mode ---
+if [[ "$USE_GAMESCOPE" == 1 ]]; then
+  exec gamescope \
     -f \
     -H 1440 \
     --mangoapp \
@@ -45,4 +49,6 @@ else
     --adaptive-sync \
     --immediate-flips \
     --backend sdl -- "$@"
+else
+  exec "$@"
 fi
