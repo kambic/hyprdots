500f38f797ac14bf8dab1b7668f19ac94683fa48
rok <rok.kambic@telekom.si>
2025-12-04 00:48:00 +0100

    u

--- firefox/link-userjs.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-PROFILE_DIR="$HOME/.mozilla/firefox"
-
-for profile in "$PROFILE_DIR"/*.default-release; do
-  ln -sf "$PWD/user.js" "$profile/user.js"
-  echo "Linked user.js to $profile"
-done
---
+++ setup/_fonts.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# --------------------------------------------------------------
+# Fonts
+# --------------------------------------------------------------
+
+sudo cp -rf $SCRIPT_DIR/fonts/FiraCode /usr/share/fonts
+sudo cp -rf $SCRIPT_DIR/fonts/Fira_Sans /usr/share/fonts
+sudo cp -rf $SCRIPT_DIR/fonts/BebasNeue-Regular /usr/share/fonts
+sudo cp -rf $SCRIPT_DIR/fonts/Material-Icons /usr/share/fonts
