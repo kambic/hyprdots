d46fd315719385ae698cada5e47211f98417951f
rok <rok.kambic@gmail.com>
2025-12-13 03:54:10 +0100

    up

--- doc/firefox/link-userjs.sh
+++ doc/firefox/link-userjs.sh
@@ -2,6 +2,6 @@
 PROFILE_DIR="$HOME/.mozilla/firefox"
 
 for profile in "$PROFILE_DIR"/*.default-release; do
-  ln -sf "$PWD/user.js" "$profile/user.js"
+  # ln -sf "$PWD/user.js" "$profile/user.js"
   echo "Linked user.js to $profile"
 done
