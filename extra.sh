358478408e6ceed8ec9bfc5069c80d4a54c4ba68
rok <rok.kambic@gmail.com>
2025-12-15 01:04:13 +0100

    bins new

--- extra.sh
+++
@@ -1,4 +0,0 @@
-# Install missing Oh My Zsh plugins
-git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
-git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
-git clone https://github.com/MichaelAquilina/zsh-you-should-use ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/you-should-use
