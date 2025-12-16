c7d2e27c15d33ce35e19c5ffd031f3c762dcfdb7
rok <rok.kambic@gmail.com>
2025-12-16 02:56:30 +0100

    up

--- setup/_fonts.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# --------------------------------------------------------------
-# Fonts
-# --------------------------------------------------------------
-
-sudo cp -rf $SCRIPT_DIR/fonts/FiraCode $HOME/.local/share/fonts
-sudo cp -rf $SCRIPT_DIR/fonts/Fira_Sans $HOME/.local/share/fonts
-sudo cp -rf $SCRIPT_DIR/fonts/BebasNeue-Regular $HOME/.local/share/fonts
-sudo cp -rf $SCRIPT_DIR/fonts/Material-Icons $HOME/.local/share/fonts
